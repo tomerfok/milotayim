bebca175f09cf92608badfca281717d55c249b1c
Tomer <103896099+tomerfok@users.noreply.github.com>
2023-12-26 23:14:29 +0200

    Update milotayim.js

--- milotayim.js
+++ milotayim.js
@@ -676,7 +676,7 @@ $(document).ready(() => {
       historicDate ? 'todays_words?date=' + historicDate : 'todays_words');
   $.get('allwords.txt').done(words => {
     allowedWords = words.split(/\r?\n/);
-    todaysWords = {'L': "הכרמל", 'R': "בורקס"}
+    todaysWords = {'L': "יצירה", 'R': "צביעה"}
     todaysNum = lastCompletedWordNum + 1;
     $('#loading-msg').hide();
     $('#start-button').show();
